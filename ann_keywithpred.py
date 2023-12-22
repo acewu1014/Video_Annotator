5f019327333fb358a74103704821ea73812efd42
acewu1014 <nemowu1999@gmail.com>
2023-12-22 15:52:32 +0800

    add keywords and genres filter

---
+++ ann_keywithpred.py
@@ -0,0 +1,223 @@
+from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QSlider, QStyle, QSizePolicy, QFileDialog, QScrollArea, QFormLayout, QGridLayout, QShortcut
+from PyQt5.QtCore import Qt
+from functools import partial
+import json 
+
+class VideoLabelandprediction(QWidget):
+    def __init__(self, videoplayer):
+        super().__init__()
+        self.file_name = ""
+        self.file_path = ""
+        self.videoplayer = videoplayer
+        self.shot_idx = 0
+        self.entries = []
+        self.keywords_origin = []
+        self.prediction_origin = []
+        self.buttons_shots = []
+        self.buttons_keywords = [[]]
+        self.buttons_predictions = [[]]
+        self.init_ui()
+        self.init_sc()
+        
+        
+    
+    def init_ui(self):
+        # self.setGeometry(350, 100, 300, 300)
+        self.scroll = QScrollArea(self)
+        # self.scroll.setGeometry(350, 100, 700, 500)
+        self.name = QLabel()
+        self.name.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+        # self.button = QPushButton("Shot: {index}".format(index = 20), self.scroll)
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        # self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.info_scroll = QScrollArea(self)
+        self.scroll.setWidgetResizable(True)
+        self.hbox = QHBoxLayout()
+        self.hbox.addWidget(self.name)
+        self.hbox.addWidget(self.scroll)
+        self.hbox.addWidget(self.info_scroll)
+        self.setLayout(self.hbox)
+        self.show()
+
+    #Used for debugging
+    def change_text(self, text):
+        self.name.setText(str(text))
+        self.name.show()
+
+
+    def open_file(self, file_path):
+        # print(self.video_name)
+        
+        if len(self.entries):
+            #save
+            self.save_file()
+        self.file_path = file_path
+        self.entries = []
+        self.buttons_shots = []
+        self.file_name = file_path.split('/')[-1].split('.')[0]
+        if file_path != '':
+            for entry in open(file_path, "r", encoding = "utf-8"):
+                entry = json.loads(entry)
+                self.entries.append(entry)
+            self.init_shot_bar()
+        #keep track of the origins, to reconvery from changes
+        # self.read_recovery()
+
+    #TODO: modify it to make it recover from original, not just the last one   
+    def read_recovery_from_idx(self, idx ):
+        if len(self.entries):
+            self.keywords_origin = self.entries[idx]['keywords'].copy()
+            self.prediction_origin = self.entries[idx]['predictions'].copy()
+        print(self.keywords_origin)
+        print(type(self.keywords_origin))
+        print(self.prediction_origin)
+        print(type(self.prediction_origin))
+
+    def clear(self):
+        for i in reversed(range(self.hbox.count())): 
+            self.hbox.itemAt(i).widget().deleteLater()
+
+    def init_shot_bar(self):
+        shot_area = QWidget()
+        form = QFormLayout()
+        for i in range(len(self.entries)):
+            entry =  self.entries[i]["boundary_timecode"]
+            button = QPushButton("Shot: {index} \nStart: {start} \nEnd: {end}".format(index = i, start = entry[0], end = entry[1]))
+            form.addRow(button)
+            self.buttons_shots.append(button)
+            # button.clicked.connect(partial(self.videoplayer.set_position, entry[0]))
+            button.clicked.connect(partial(self.videoplayer.play_segment, entry))
+            button.clicked.connect(partial(self.init_shot_info, i))
+            button.clicked.connect(partial(self.change_now_shot_index, i))
+            button.clicked.connect(partial(self.change_color_shot, button))
+            button.clicked.connect(partial(self.read_recovery_from_idx, i))
+        # self.scroll.show()
+        shot_area.setLayout(form)
+        self.scroll.setWidget(shot_area)
+        # hbox = QHBoxLayout()
+        print(self.hbox.count())
+        # self.hbox.addWidget(self.scroll)
+        # hbox.addWidget(self.scroll)
+        self.setLayout(self.hbox)
+        self.show()
+
+    def init_sc(self):
+        self.sc_pause = QShortcut("C", self)
+        self.sc_pause.activated.connect(self.copy_former_one)
+
+        self.sc_recovery = QShortcut("R", self)
+        self.sc_recovery.activated.connect(self.recovery)
+
+    def change_now_shot_index(self, j):
+        self.shot_idx = j
+
+    def init_shot_info(self, j):
+        #List the info of No.j entry
+        info_area = QWidget()
+        entry = self.entries[j]
+        keywords = entry["keywords"]
+        predictions = entry["predictions"]
+        grid = QGridLayout()
+        label1 = QLabel("keywords")
+        label2 = QLabel("Predictions")
+        grid.addWidget(label1, 0, 0)
+        grid.addWidget(label2, 0, 1)
+        #Create buttons for each tag
+        #Original keywords
+        for i in range(len(keywords)):
+            button = QPushButton(keywords[i])
+            grid.addWidget(button, i+1, 0)
+            self.buttons_keywords.append(button)
+            button.clicked.connect(partial(self.delete_keywords, j, keywords[i]))
+            button.clicked.connect(partial(self.change_color, button))
+        # Model prediction
+        for i in range(len(predictions)):
+            button = QPushButton(predictions[i])
+            grid.addWidget(button, i+1, 1)
+            self.buttons_predictions.append(button)
+            button.clicked.connect(partial(self.delete_predictions, j, predictions[i]))
+            button.clicked.connect(partial(self.change_color, button))
+    
+
+        # Add select all function
+        # selectall_keywords = QPushButton("Select all")
+        # selectall_genres = QPushButton("Select all")
+        # grid.addWidget(selectall_keywords, 0, 1)
+        # grid.addWidget(selectall_genres, 0, 5)
+        # selectall_keywords.clicked.connect(partial(self.delete_all_keywords, grid, j))
+        # selectall_genres.clicked.connect(partial(self.delete_all_genres, grid, j))
+        info_area.setLayout(grid)
+        self.info_scroll.setWidget(info_area)
+        info_area = QWidget()
+        # form = QFormLayout()
+        
+    #TODO: delete button and corresponding entry
+    def delete_keywords(self, index, keyword_name):
+        # self.entries[index]['keywords'] = self.entries[index]['keywords'].remove(keyword_name)
+        if keyword_name in self.entries[index]['keywords']:
+            self.entries[index]['keywords'].remove(keyword_name)
+        else:
+            self.entries[index]['keywords'].append(keyword_name)
+
+    def delete_predictions(self, index, prediction_name):
+        # self.entries[index]['keywords'] = self.entries[index]['keywords'].remove(keyword_name)
+        if prediction_name in self.entries[index]['predictions']:
+            self.entries[index]['predictions'].remove(prediction_name)
+        else:
+            self.entries[index]['predictions'].append(prediction_name)
+
+    
+    def change_color(self, button):
+        if button.palette().button().color().value() != 255:
+            button.setStyleSheet("background-color: red")
+        else:
+            button.setStyleSheet("background-color: light gray")
+    
+    def change_color_red(self, button):
+        button.setStyleSheet("background-color: red")
+
+    def change_color_shot(self, button):
+        if button.palette().button().color().value() != 255:
+            button.setStyleSheet("background-color: green")
+        # else:
+        #     button.setStyleSheet("background-color: light gray")
+
+    def delete_all_keywords(self, grid, index):
+        self.entries[index]['keywords'] = []
+        for i in range(1, grid.rowCount()):
+            button = grid.itemAtPosition(i, 0)
+            if button:
+                button = button.widget()
+                self.change_color_red(button)
+
+    def delete_all_genres(self, grid, index):
+        self.entries[index]['genres'] = []
+        for i in range(1, grid.rowCount()):
+            button = grid.itemAtPosition(i, 4)
+            if button:
+                button = button.widget()
+                self.change_color_red(button)
+
+    def save_file(self):
+        with open(self.file_path , 'w',encoding="utf-8") as outputfile:
+            for entry in self.entries:
+                json.dump(entry, outputfile, ensure_ascii=False)
+                outputfile.write('\n')
+            print("Successfully save file!")
+
+    #copy keywords and genres from the former shot(i.e. i-1) 
+    def copy_former_one(self):
+        if self.shot_idx>0:
+            self.entries[self.shot_idx]['keywords'] = self.entries[self.shot_idx - 1]['keywords'].copy()
+            self.entries[self.shot_idx]['predictions'] = self.entries[self.shot_idx - 1]['genres'].copy()
+            self.init_shot_info(self.shot_idx)
+            print("Copy succ")
+            print(self.shot_idx)
+        else:
+            print("now is shot 0")
+
+    def recovery(self):
+        self.entries[self.shot_idx]['keywords'] = self.keywords_origin.copy()
+        self.entries[self.shot_idx]['predictions'] = self.prediction_origin.copy()
+        self.init_shot_info(self.shot_idx)
+        print("Recovery from the last shot")
--- start.py
+++ start.py
@@ -3,17 +3,21 @@ from VideoPlayer import VideoPlayer
 from PyQt5.QtGui import QIcon, QPalette
 from PyQt5 import QtGui
 from PyQt5.QtCore import  Qt, QRect
-from load_json import VideoLabel
+from ann_keyword import VideoLabel
+from ann_keywithpred import VideoLabelandprediction
+from genres_load_json import VideoGenresLabel
 from functools import partial
+
 class container(QMainWindow):
     def __init__(self):
         super().__init__()
         self.videoplayer = VideoPlayer()
         self.videolabel = VideoLabel(self.videoplayer)
+        # self.videolabel = VideoLabelandprediction(self.videoplayer)
         self.setWindowTitle("Video Annotator")
         self.setGeometry(350, 100, 900, 800)
         self.setWindowIcon(QIcon('window_icon.png'))
-        
+        self.system = 1
         self.init_menubar()
         self.init_ui()
     
@@ -21,11 +25,16 @@ class container(QMainWindow):
         p = self.palette()
         p.setColor(QPalette.Window, Qt.white)
         self.setPalette(p)
-        self.test_label = QLabel()
-        self.test_label.setText("Json loader")
-        self.test_label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+        self.system_label = QLabel()
+        self.system_label.setText("System: " + "Keywords Annotate")
+        self.system_label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+        self.vboxLayout = QVBoxLayout()
+        self.vboxLayout.addWidget(self.system_label)
+        self.vboxLayout.addWidget(self.videoplayer)
+        self.mid = QWidget()
+        self.mid.setLayout(self.vboxLayout)
         self.hboxLayout = QHBoxLayout()
-        self.hboxLayout.addWidget(self.videoplayer)
+        self.hboxLayout.addWidget(self.mid)
         self.hboxLayout.addWidget(self.videolabel)
         wid = QWidget()
         wid.setLayout(self.hboxLayout)
@@ -36,16 +45,25 @@ class container(QMainWindow):
         # menubar = QMenuBar(self)
         menubar = self.menuBar()
         menu_file = QMenu('FILE', self)
+        menu_system = QMenu('SYSTEM', self)
         menubar.addMenu(menu_file)
+        menubar.addMenu(menu_system)
         action_open = QAction('OPEN VIDEO', self)
         action_save = QAction('SAVE LABEL', self)
         # action_open_json = QAction('OPEN LABEL')
-    
+        action_genrefilter = QAction('GENRE FILTER', self)
+        action_keywords_annotate = QAction('KEYWORDS ANNOTATE', self)
+        action_keywords_filter = QAction('KEYWORDS FILTER', self)
         menu_file.addAction(action_open)
         menu_file.addAction(action_save)
+        menu_system.addAction(action_genrefilter)
+        menu_system.addAction(action_keywords_annotate)
+        menu_system.addAction(action_keywords_filter)
         # menu_file.addAction(action_open_json)
         # add an open file corresponding action
-        
+        action_genrefilter.triggered.connect(self.change_genre_filter)
+        action_keywords_annotate.triggered.connect(self.change_keyword_annotate)
+        action_keywords_filter.triggered.connect(self.change_keyword_filter)
         action_open.triggered.connect(self.open_file)
         action_save.triggered.connect(self.videolabel.save_file)
         # action_open_json.triggered.connect(videolabel.open_file)
@@ -57,6 +75,7 @@ class container(QMainWindow):
         # menubar.setGeometry(QRect(0, 0, 878, 20))
         # self.setMenuBar(menubar)
 
+
     def open_file(self):
         file_path, _ = QFileDialog.getOpenFileName(self, "Open Video")
 
@@ -65,6 +84,38 @@ class container(QMainWindow):
         label_file_path += '.json'
         self.videolabel.open_file(label_file_path)
     
+    def change_keyword_annotate(self):
+        self.videoplayer = VideoPlayer()
+        self.videolabel = VideoLabel(self.videoplayer)
+        self.setWindowTitle("Video Annotator")
+        self.setGeometry(350, 100, 900, 800)
+        
+        self.init_ui()
+
+        self.system_label.setText("System: " + "Keywords Annotate")
+        self.show()
+
+    def change_keyword_filter(self):
+        self.videoplayer = VideoPlayer()
+        self.videolabel = VideoLabelandprediction(self.videoplayer)
+        self.setWindowTitle("Video Annotator")
+        self.setGeometry(350, 100, 900, 800)
+    
+        self.init_ui()
+
+        self.system_label.setText("System: " + "Keywords Filter")
+        self.show()
+
+    def change_genre_filter(self):
+        self.videoplayer = VideoPlayer()
+        self.videolabel = VideoGenresLabel(self.videoplayer)
+        self.setWindowTitle("Video Annotator")
+        self.setGeometry(350, 100, 900, 800)
+    
+        self.init_ui()
+
+        self.system_label.setText("System: " + "Genre Filter")
+        self.show()
 
 
 if __name__ == '__main__':
